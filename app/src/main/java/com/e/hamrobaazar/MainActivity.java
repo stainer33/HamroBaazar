2152a2e9c507d77cffaef8ac4cb8b2c46d8e3c56
stainer33 <spasang91@gmail.com>
2020-01-02 21:52:42 +0545

    navigation drawer done

--- app/src/main/java/com/e/hamrobaazar/MainActivity.java
+++ app/src/main/java/com/e/hamrobaazar/MainActivity.java
@@ -1,6 +1,9 @@
 package com.e.hamrobaazar;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,12 +15,22 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     RecyclerView itemsRecyclerView;
 
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout=findViewById(R.id.drawerLayout);
+        toolbar=findViewById(R.id.toolbar);
         itemsRecyclerView=findViewById(R.id.itemsRecyclerView);
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle actionBarDrawerToggle=new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.start,R.string.end);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        /*
 
         List<Item>items=new ArrayList<>();
         items.add(new Item("pulsar",R.drawable.cat,1000,"Used"));
@@ -27,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         ItemAdapter itemAdapter=new ItemAdapter(this,items);
         itemsRecyclerView.setAdapter(itemAdapter);
-        itemsRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        itemsRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));*/
 
     }
 }
